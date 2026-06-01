7cbdf14b7a9a33179bacca5712c40fb562e3ff49
Evelyne Albero <evelyne.albero@gmail.com>
2026-06-01 11:28:54 +0200

    added link to homepage and date/time current location

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,5 @@
+let selectedCityTimeZone = null;
+
 function updateTime() {
   let losAngelesElement = document.querySelector("#los-angeles");
   if (losAngelesElement) {
@@ -34,25 +36,38 @@ function updateTime() {
   }
 }
 
-function updateCity(event) {
-  let cityTimeZone = event.target.value;
+function updateSelectedCity() {
+  if (!selectedCityTimeZone) {
+    return;
+  }
+
+  let cityTimeZone = selectedCityTimeZone;
   if (cityTimeZone === "current") {
     cityTimeZone = moment.tz.guess();
   }
-  let cityName = cityTimeZone.replace("_", "").split("/")[1];
+
+  let cityName = cityTimeZone.split("/").pop().replace(/_/g, " ");
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
+
   citiesElement.innerHTML = `<div class="city">
         <div>
         <h2>${cityName} 🌍</h2>
         <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
         </div>
-        <div class="time">${cityTime.format("h:mm:ss")}<small>${cityTime.format("A")}</small></div>
-    </div>`;
+        <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format("A")}</small></div>
+    </div>
+    <a href="index.html" class="home-link">All cities</a>`;
+}
+
+function updateCity(event) {
+  selectedCityTimeZone = event.target.value;
+  updateSelectedCity();
 }
 
 updateTime();
 setInterval(updateTime, 1000);
+setInterval(updateSelectedCity, 1000);
 
 let citiesSelectElement = document.querySelector("#city");
 citiesSelectElement.addEventListener("change", updateCity);
